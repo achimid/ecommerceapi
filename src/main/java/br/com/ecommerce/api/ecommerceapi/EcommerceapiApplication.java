1b96094cc6647144ff0a3d71b24c5824653bc171
Lourran Olimpio Zorzi <achimid@hotmail.com>
2017-07-11 20:11:30 -0300

    Initial commit

---
+++ src/main/java/br/com/ecommerce/api/ecommerceapi/EcommerceapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.ecommerce.api.ecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceapiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/ecommerce/api/ecommerceapi/EcommerceapiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ecommerce.api.ecommerceapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcommerceapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
