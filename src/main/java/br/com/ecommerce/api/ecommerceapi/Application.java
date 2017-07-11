8f093f5b9a555a5c7b14ee0ff0b5fb4d373c4977
Lourran Olimpio Zorzi <achimid@hotmail.com>
2017-07-11 20:26:35 -0300

    v2

--- src/main/java/br/com/ecommerce/api/ecommerceapi/Application.java
+++ src/main/java/br/com/ecommerce/api/ecommerceapi/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceapiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceapiApplication.class, args);
+		SpringApplication.run(Application.class, args);
+		System.out.println(1111);
 	}
 }
---
+++ src/main/java/br/com/ecommerce/api/ecommerceapi/domain/BaseDomain.java
@@ -0,0 +1,35 @@
+package br.com.ecommerce.api.ecommerceapi.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+/**
+ * Created by Lourran on 11/07/2017.
+ */
+@Data
+@MappedSuperclass
+public class BaseDomain {
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(nullable = false)
+    protected LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(nullable = false)
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
