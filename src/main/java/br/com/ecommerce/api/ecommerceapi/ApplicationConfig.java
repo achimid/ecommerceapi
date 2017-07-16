192e7ebb5ad5f916c864211bcef853ac68759888
Lourran Olimpio Zorzi <achimid@hotmail.com>
2017-07-15 23:25:18 -0300

    v7 Layout

---
+++ src/main/java/br/com/ecommerce/api/ecommerceapi/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package br.com.ecommerce.api.ecommerceapi;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Lourran on 15/07/2017.
+ */
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container ->
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
+                        new ErrorPage(HttpStatus.FORBIDDEN, "/403")));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+}
